6fd70002edb31b7f444d6e3f040db725584efc03
[Abhaya] <[gopalkumarshrestha123@gmail.com]>
2024-07-19 16:50:37 +0545

    My portfolio

---
+++ src/data.js
@@ -0,0 +1,16 @@
+const data = {
+    name: "Abhaya Shrestha",
+    githubLink: "https://github.com/GITHUB_USERNAME" ,
+    email : "gopalkumarshrestha123@gmail.com",
+    hobbies : [
+        "Travelling",
+        "Movies",
+        "Football"
+    ],
+    jobTitle : "Software Engineer",
+    degree: "Bachelor's in Computer Engineering",
+    campus: "Pulchowk Campus",
+    location: "Nepal",
+    spokenLanguages: "Nepali,English",
+}
+export default data;
